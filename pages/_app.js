8ba540117bf92f4d972f2cf92d898f53f7ad7a2c
Carvel Russ <russserafin15@gmail.com>
2025-03-11 18:42:46 +0800

    Updated the logo

--- pages/_app.js
+++ pages/_app.js
@@ -1,61 +1,59 @@
-import dynamic from 'next/dynamic'
-import Router from 'next/router'
-import Head from 'next/head'
-import NProgress from 'nprogress'
-import ScrollTopButton from '../components/ScrollTopButton'
-import '../scss/theme.scss'
+import dynamic from "next/dynamic";
+import Router from "next/router";
+import Head from "next/head";
+import NProgress from "nprogress";
+import ScrollTopButton from "../components/ScrollTopButton";
+import "../scss/theme.scss";
 
-const SafeSSRProvider = dynamic(() => import('react-bootstrap/SSRProvider'), {
+const SafeSSRProvider = dynamic(() => import("react-bootstrap/SSRProvider"), {
   ssr: false,
-})
+});
 
 const Finder = ({ Component, pageProps }) => {
   // Bind NProgress to Next Router events (Page loading animation)
-  Router.events.on('routeChangeStart', () => NProgress.start())
-  Router.events.on('routeChangeComplete', () => NProgress.done())
-  Router.events.on('routeChangeError', () => NProgress.done())
+  Router.events.on("routeChangeStart", () => NProgress.start());
+  Router.events.on("routeChangeComplete", () => NProgress.done());
+  Router.events.on("routeChangeError", () => NProgress.done());
 
   return (
     <SafeSSRProvider>
       <Head>
-        <meta name='viewport' content='width=device-width, initial-scale=1' />
-        <title>
-          Finder | Directory &amp; Listings React / Next.js Bootstrap Template
-        </title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Scoutlify | Real Estate Directory</title>
         <meta
-          name='description'
-          content='Finder - Directory &amp; Listings React / Next.js Bootstrap Template'
+          name="description"
+          content="Finder - Directory &amp; Listings React / Next.js Bootstrap Template"
         />
         <meta
-          name='keywords'
-          content='react, nextjs, bootstrap, business, directory, listings, e-commerce, car dealer, city guide, real estate, job board, user account, multipurpose, ui kit, css3, javascript, gallery, slider, touch'
+          name="keywords"
+          content="react, nextjs, bootstrap, business, directory, listings, e-commerce, car dealer, city guide, real estate, job board, user account, multipurpose, ui kit, css3, javascript, gallery, slider, touch"
         />
-        <meta name='author' content='Createx Studio' />
+        <meta name="author" content="Createx Studio" />
         <link
-          rel='apple-touch-icon'
-          sizes='180x180'
-          href='/favicon/apple-touch-icon.png'
-        />
-        <link
-          rel='icon'
-          type='image/png'
-          sizes='32x32'
-          href='/favicon/favicon-32x32.png'
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon/apple-touch-icon.png"
         />
         <link
-          rel='icon'
-          type='image/png'
-          sizes='16x16'
-          href='/favicon/favicon-16x16.png'
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon/favicon-32x32.png"
         />
-        <link rel='manifest' href='/favicon/site.webmanifest' />
         <link
-          rel='mask-icon'
-          color='#5bbad5'
-          href='/favicon/safari-pinned-tab.svg'
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon/favicon-16x16.png"
         />
-        <meta name='msapplication-TileColor' content='#766df4' />
-        <meta name='theme-color' content='#ffffff' />
+        <link rel="manifest" href="/favicon/site.webmanifest" />
+        <link
+          rel="mask-icon"
+          color="#5bbad5"
+          href="/favicon/safari-pinned-tab.svg"
+        />
+        <meta name="msapplication-TileColor" content="#766df4" />
+        <meta name="theme-color" content="#ffffff" />
       </Head>
 
       <Component {...pageProps} />
@@ -63,11 +61,11 @@ const Finder = ({ Component, pageProps }) => {
       <ScrollTopButton
         showOffset={600}
         duration={800}
-        easing='easeInOutQuart'
-        tooltip='Top'
+        easing="easeInOutQuart"
+        tooltip="Top"
       />
     </SafeSSRProvider>
-  )
-}
+  );
+};
 
-export default Finder
+export default Finder;
