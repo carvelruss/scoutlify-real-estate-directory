c5a87b832cfb82db98edf9a0df9711186c750cf6
carvelruss <russserafin15@gmail.com>
2025-04-10 05:15:01 +0800

    .

--- next.config.js
+++ next.config.js
@@ -1,16 +1,18 @@
-/** @type {import('next').NextConfig} */
-
-module.exports = {
-  reactStrictMode: true,
-  images: {
-    imageSizes: [48, 64, 88, 96, 128, 256, 384, 416],
-  }
-}
-
+// Import the bundle analyzer
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
 
 module.exports = withBundleAnalyzer({
   reactStrictMode: true,
+  images: {
+    imageSizes: [48, 64, 88, 96, 128, 256, 384, 416],
+  },
+  webpack(config, { isServer }) {
+    if (!isServer) {
+      // Adjust max chunk size for the client-side build
+      config.optimization.splitChunks.maxSize = 250000; // Adjust chunk size as needed
+    }
+    return config;
+  },
 });
